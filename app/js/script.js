b91a8439f480e7db3a0ec8f13e386652c475d049
Stupak Serhii <stupak.s.v1337@gmail.com>
2025-06-15 20:05:30 +0300

    mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,15 @@
 $('.js-slider').slick({
-  infinite: true,
   slidesToShow: 1.5,
   slidesToScroll: 1,
   infinite: false,
   arrows: false,
+});
+
+$('.js-slider-man').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: false,
+  arrows: false,
+  dots: true,
+  dotsClass: 'slider-man__dots',
 });
