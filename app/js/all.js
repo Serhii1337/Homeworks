5fcd3cb84dcad50188600eae4a1a5f890423e23f
Stupak Serhii <stupak.s.v1337@gmail.com>
2025-06-16 19:28:50 +0300

    adaptation

--- app/js/all.js
+++ app/js/all.js
@@ -4032,3 +4032,40 @@ $('.js-slider-man').slick({
   dots: true,
   dotsClass: 'slider-man__dots'
 });
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('.header');
+  var btn = document.querySelector('.header__btn');
+  var menuList = document.querySelector('.header__list');
+  var scrollPosition = 0;
+  btn.addEventListener('click', function (e) {
+    e.stopPropagation();
+    if (!header.classList.contains('active')) {
+      scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      document.body.style.top = "-".concat(scrollPosition, "px");
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+      document.body.classList.add('menu-open');
+    } else {
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollPosition);
+      document.body.classList.remove('menu-open');
+    }
+    header.classList.toggle('active');
+  });
+  document.addEventListener('click', function (e) {
+    var isClickInside = header.contains(e.target);
+    if (!isClickInside && header.classList.contains('active')) {
+      header.classList.remove('active');
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollPosition);
+      document.body.classList.remove('menu-open');
+    }
+  });
+  menuList.addEventListener('click', function (e) {
+    e.stopPropagation();
+  });
+});
--- app/js/script.js
+++ app/js/script.js
@@ -13,3 +13,44 @@ $('.js-slider-man').slick({
   dots: true,
   dotsClass: 'slider-man__dots',
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('.header');
+  const btn = document.querySelector('.header__btn');
+  const menuList = document.querySelector('.header__list');
+  let scrollPosition = 0;
+
+  btn.addEventListener('click', function (e) {
+    e.stopPropagation();
+
+    if (!header.classList.contains('active')) {
+      scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      document.body.style.top = `-${scrollPosition}px`;
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+      document.body.classList.add('menu-open');
+    } else {
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollPosition);
+      document.body.classList.remove('menu-open');
+    }
+
+    header.classList.toggle('active');
+  });
+  document.addEventListener('click', function (e) {
+    const isClickInside = header.contains(e.target);
+    if (!isClickInside && header.classList.contains('active')) {
+      header.classList.remove('active');
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollPosition);
+      document.body.classList.remove('menu-open');
+    }
+  });
+  menuList.addEventListener('click', function (e) {
+    e.stopPropagation();
+  });
+});
