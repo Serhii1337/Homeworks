d81ac776d00a4bad4c4f38b9d82af68c758502af
Stupak Serhii <stupak.s.v1337@gmail.com>
2025-06-17 20:59:21 +0300

    about

--- app/js/all.js
+++ app/js/all.js
@@ -4018,11 +4018,52 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
     return o;
   };
 });
-$('.js-slider').slick({
-  slidesToShow: 1.5,
+$('.js-achievements__slider').slick({
+  slidesToShow: 1,
   slidesToScroll: 1,
   infinite: false,
-  arrows: false
+  arrows: false,
+  mobileFirst: true,
+  dots: true,
+  dotsClass: 'slider-man__dots',
+  centerMode: true,
+  centerPadding: '0',
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 4,
+      centerMode: false,
+      slidesToScroll: 2
+    }
+  }]
+});
+$(function () {
+  var $slider = $('.js-slider');
+  var isSlickInitialized = false;
+  function initSlick() {
+    var windowWidth = $(window).width();
+    if (windowWidth >= 992) {
+      if (isSlickInitialized) {
+        $slider.slick('unslick');
+        isSlickInitialized = false;
+      }
+    } else {
+      if (!isSlickInitialized) {
+        $slider.slick({
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          infinite: false,
+          arrows: false,
+          mobileFirst: true
+        });
+        isSlickInitialized = true;
+      }
+    }
+  }
+  initSlick();
+  $(window).on('resize', function () {
+    initSlick();
+  });
 });
 $('.js-slider-man').slick({
   slidesToShow: 1,
--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,57 @@
-$('.js-slider').slick({
-  slidesToShow: 1.5,
+$('.js-achievements__slider').slick({
+  slidesToShow: 1,
   slidesToScroll: 1,
   infinite: false,
   arrows: false,
+  mobileFirst: true,
+  dots: true,
+  dotsClass: 'slider-man__dots',
+  centerMode: true,
+  centerPadding: '0',
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        centerMode: false,
+        slidesToScroll: 2,
+      }
+    }
+  ]
+});
+
+$(function () {
+  const $slider = $('.js-slider');
+  let isSlickInitialized = false;
+
+  function initSlick() {
+    const windowWidth = $(window).width();
+
+    if (windowWidth >= 992) {
+      if (isSlickInitialized) {
+        $slider.slick('unslick');
+        isSlickInitialized = false;
+      }
+    } else {
+      if (!isSlickInitialized) {
+        $slider.slick({
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          infinite: false,
+          arrows: false,
+          mobileFirst: true,
+        });
+        isSlickInitialized = true;
+      }
+    }
+  }
+
+  initSlick();
+
+  $(window).on('resize', function () {
+    initSlick();
+  });
 });
 
 $('.js-slider-man').slick({
